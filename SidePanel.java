c466dd7f2c21019016b6a07f200823104c22c048
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2022-08-05 12:48:50 -0700

    Getting information seems to work well - now redo graphs whenever it is

--- SidePanel.java
+++ SidePanel.java
@@ -24,15 +24,15 @@ public class SidePanel extends JPanel implements ActionListener{
 		fields = new ArrayList<>();
 
 		//Add physics fields
-		fields.add(new MyField("t"));
-		fields.add(new MyField("v"));
-		fields.add(new MyField("s"));
-		fields.add(new MyField("a"));
+		fields.add(new MyField("t", car.t));
+		fields.add(new MyField("v", car.v));
+		fields.add(new MyField("s", car.s));
+		fields.add(new MyField("a", car.a));
 
 		//Add PID fields
-		fields.add(new MyField("P"));
-		fields.add(new MyField("I"));
-		fields.add(new MyField("D"));
+		fields.add(new MyField("P", car.kP));
+		fields.add(new MyField("I", car.kI));
+		fields.add(new MyField("D", car.kD));
 	
 		addFieldsToPanel();
 		addActionListeners();
@@ -40,48 +40,58 @@ public class SidePanel extends JPanel implements ActionListener{
 
 	@Override
         public void actionPerformed(ActionEvent e) {
+		//Update all physics values
+		car.t = fields.get(0).getVal();
+		car.v = fields.get(1).getVal();
+		car.s = fields.get(2).getVal();
+		car.a = fields.get(3).getVal();
 
-		for (int x = 0; x < fields.size(); x++){
-			if (e.getSource() == fields.get(x).field){
+		//Update all PID values
+		car.kP = fields.get(4).getVal();
+		car.kI = fields.get(5).getVal();
+		car.kD = fields.get(6).getVal();
 
-				//C++ references to integers would be handy here
-
-			}
-		}
-        }
+		
+	}
 
 	//Adds all MyFields to panel
 	public void addFieldsToPanel(){
+		add(new JLabel("Physics Values:"));
+
 		for (int x = 0; x < fields.size(); x++){
 			add(fields.get(x));
+
+			if (fields.get(x).text.equals("a")){
+				add(new JLabel("PID Values:"));
+			}
 		}
 	}
 
 	//Adds action listeners to MyField fields
 	public void addActionListeners(){
 		for (int x = 0; x < fields.size(); x++){
-                        fields.get(x).addActionListener(this);
+                        fields.get(x).field.addActionListener(this);
                 }
 	}
-
-
 
 	public class MyField extends JPanel{
 		public JTextField field;
 		public JLabel label;
+		public String text;
 
-		public MyField(String text){
+		public MyField(String text, double val){
+			this.text = text;
 			label = new JLabel(" " + text + ": ");
-			field = new JTextField(10);
+			field = new JTextField(String.valueOf(val), 10);
 
 			setLayout(new BorderLayout());
 			add(label, BorderLayout.WEST);
 			add(field, BorderLayout.CENTER);
-
 		}
 
-
-
-		
+		//Return val from field
+		public double getVal(){
+			return Double.parseDouble(field.getText());
+		}
 	}
 }
