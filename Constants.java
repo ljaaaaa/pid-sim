bafc914ab98f9d634a51d5de8dc0eeed76eecdb9
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2022-08-28 11:49:09 -0700

    Removed variable in Constants

--- Constants.java
+++ Constants.java
@@ -14,6 +14,5 @@ public class Constants {
 	public static final Color GREY_HIGH = new Color(150, 150, 150);
 	public static final Color BLACK = new Color(45, 45, 45);
 	public static final Color POINT = new Color(180, 0, 0);
-	public static final Color CAR = new Color(23, 100, 45);
 	public static final Color TARGET = new Color(0, 0, 255);
 }
